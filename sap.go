1591ac3aa8310e00569baea8690d324585de89ab
Mateo Rodriguez <teorodrip@posteo.net>
2020-04-17 21:32:42 +0200

    start on sorting axes

--- sap.go
+++ sap.go
@@ -8,19 +8,43 @@
 
 package main
 
-import (
-	"github.com/faiface/pixel"
-)
+// import "github.com/faiface/pixel"
 
 type EndPoint struct {
 	Data  uint16
 	Value float64
 }
 
+func (e EndPoint) isMin() bool {
+	return (e.Data & 0x1) == 0x1
+}
+
+type SapAxis struct {
+	X, Y *EndPoint
+}
+
 type Box struct {
-	Min [2]*EndPoint
-	Max [2]*EndPoint
+	Min SapAxis
+	Max SapAxis
 }
 
 type SweepPrune struct {
+	EndPointsX []EndPoint
+	EndPointsY []EndPoint
+}
+
+func SortEndPoints(endpoints []EndPoint) {
+	var i int
+	var swapper EndPoint
+
+	for j, endpoint := range endpoints {
+		val := endpoint.Value
+		i = j - 1
+		for i >= 0 && endpoints[i].Value > val {
+			swapper = endpoints[i]
+			if endpoint.isMin() && !swapper.isMin() {
+				// TODO check overlap and write to pair array
+			}
+		}
+	}
 }
